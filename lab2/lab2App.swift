04f8ec80933d852daaf7be108b0123a0d834571a
Zkaria <ZkariaDiab308>
2026-03-09 01:01:22 +0100

    Initial Commit

---
+++ lab2/lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  lab2App.swift
+//  lab2
+//
+//  Created by Zkaria Diab on 2026-03-09.
+//
+
+import SwiftUI
+
+@main
+struct lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
